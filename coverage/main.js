334489f09074a448378659dfdc2470a48ec74360
JoaoColonna <JoaoColonna@users.noreply.github.com>
2025-09-14 23:37:02 +0000

    deploy: 76bfe27fb80e7e2d175c68e4f68dd9b21d1a683f

--- coverage/main.js
+++ coverage/main.js
@@ -213,7 +213,7 @@ var assemblies = [
   {
     "name": "RegisterAPI",
     "classes": [
-      { "name": "Program", "rp": "RegisterAPI_Program.html", "cl": 0, "ucl": 100, "cal": 100, "tl": 166, "cb": 0, "tb": 34, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
+      { "name": "Program", "rp": "RegisterAPI_Program.html", "cl": 0, "ucl": 104, "cal": 104, "tl": 171, "cb": 0, "tb": 34, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "RegisterAPI.Controllers.AuthController", "rp": "RegisterAPI_AuthController.html", "cl": 22, "ucl": 0, "cal": 22, "tl": 76, "cb": 8, "tb": 8, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "RegisterAPI.Controllers.PersonController", "rp": "RegisterAPI_PersonController.html", "cl": 14, "ucl": 0, "cal": 14, "tl": 107, "cb": 2, "tb": 2, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
       { "name": "RegisterAPI.Controllers.PersonV2", "rp": "RegisterAPI_PersonV2.html", "cl": 0, "ucl": 14, "cal": 14, "tl": 107, "cb": 0, "tb": 2, "cm": 0, "fcm": 0, "tm": 0, "lch": [], "bch": [], "mch": [], "mfch": [], "hc": [], "metrics": { } },
